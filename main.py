0ae1cf1e912aaa22c709f4934c0bedf44a84e45a
AhmadAmali <ahmadamali77@gmail.com>
2021-06-21 21:21:31 -0600

    implemented simple project structure

---
+++ main.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="Halal-Hacks",
+    version='0.1',
+    url='',
+    description='',
+    author='Iqraa',
+    author_email='',
+    packages=["project"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
