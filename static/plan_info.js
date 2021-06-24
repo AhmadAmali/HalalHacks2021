edf573b017678324513458413f868011aa045f25
OmarDajani75 <omardajani079@gmail.com>
2021-06-24 10:52:01 -0700

    additions to plan pages

---
+++ static/plan_info.js
@@ -0,0 +1,9 @@
+let post= {
+	0:{
+		title:"Bunyaan",
+		author:"Margot Robbie",
+		num_comments: 75,
+		num_likes: 132,
+		post_content:"Stream Planet Her on June 25th. Doja Cat rules!!!!!!"
+	}
+}
