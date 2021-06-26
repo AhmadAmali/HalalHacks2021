0babb60a25fc45c7b2f1a8a7739a94ac9b06550c
lelzeiny <leena.elzeiny@gmail.com>
2021-06-25 19:26:09 -0700

    tab switching for plan_info, empty new_plan files

--- static/plan_info.js
+++ static/plan_info.js
@@ -1,1 +1,13 @@
-const tabBar = mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
+const tabBar = mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.mdc-tab-bar'));
+
+$(".mdc-top-app-bar--fixed-adjust").on("MDCTabBar:activated", function(event){
+    console.log(event.detail);
+    if(event.detail.index == 0){
+        $(".info-container").hide();
+        $(".feed-container").show();
+    }
+    else{
+        $(".feed-container").hide();
+        $(".info-container").show();
+    }
+});
